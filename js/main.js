52c92e918ff87b05674e3487fb23510f6e140e33
connieduong <gaconnie@Connies-MacBook-Pro-2.local>
2017-05-15 18:13:25 -0700

    created game logioc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
+// cards  array
 var cards = ["queen" + "queen" + "king" + "king"];
+
+// cardsInPlay array
 var cardsInPlay = [cards.push('cardOne') + cards.push('cardTwo')];
-var cardOne = cards[0];
-console.log("User flipped queen");
-var cardTwo = cards[2];
-console.log("User flipped king")
+
+function checkForMatch () {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+}
+// checkForMatch();
+
+var flipCard = function() {
+	var cardId = ();
+	console.log("User flipped " + card[cardId]);
+	var cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+
+
+	
+
 
 var cards = [];
 if ( cardsInPlay.length === 2) {
@@ -13,4 +35,9 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 } else { 
 	console.log("Sorry, try again.");
 	alert("Sorry, try again.");
-} }
+} }) {
+	
+}
+
+
+
