a61e80c45008244729b961f3c595fee37c0d663b
Connie Duong <connieduong@gmail.com>
2017-05-21 19:40:51 -0700

    created game logica

--- js/main.js
+++ js/main.js
@@ -1,43 +1,17 @@
-// cards  array
-var cards = ["queen" + "queen" + "king" + "king"];
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [cardOne, cardTwo];
 
-// cardsInPlay array
-var cardsInPlay = [cards.push('cardOne') + cards.push('cardTwo')];
+var cardOne = [cards[0]];
+cardsInPlay.push(cards);
 
-function checkForMatch () {
+var cardTwo = [cards[2]];
+cardsInPlay.push(cards);
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
-  } else {
-      console.log("Sorry, try again.");
-  }
-}
-// checkForMatch();
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 
-var flipCard = function() {
-	var cardId = ();
-	console.log("User flipped " + card[cardId]);
-	var cardsInPlay.push(cards[cardId]);
-	checkForMatch();
-}
-flipCard(0);
-flipCard(2);
-
-
-	
-
-
-var cards = [];
-if ( cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else { 
-	console.log("Sorry, try again.");
-	alert("Sorry, try again.");
-} }) {
-	
+	alert("Sorry, try again");
 }
-
-
-
